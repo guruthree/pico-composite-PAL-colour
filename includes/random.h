2c83471098a192dfb9182bc964c649672b9e65dc
Fred <guru.three@gmail.com>
2022-03-22 22:41:53 +0000

    break out random functions into their own header

---
+++ includes/random.h
@@ -0,0 +1,44 @@
+/*
+ *
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2022 guruthree
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ */
+
+// https://forums.raspberrypi.com/viewtopic.php?t=302960
+void seed_random_from_rosc()
+{
+  uint32_t random = 0x811c9dc5;
+  uint8_t next_byte = 0;
+  volatile uint32_t *rnd_reg = (uint32_t *)(ROSC_BASE + ROSC_RANDOMBIT_OFFSET);
+
+  for (int i = 0; i < 16; i++) {
+    for (int k = 0; k < 8; k++) {
+      next_byte = (next_byte << 1) | (*rnd_reg & 1);
+    }
+
+    random ^= next_byte;
+    random *= 0x01000193;
+  }
+
+  srand(random);
+} 
--- pico-composite-PAL-colour.cpp
+++ pico-composite-PAL-colour.cpp
@@ -59,37 +59,8 @@ inline void dmacpy(uint8_t *dst, uint8_t *src, uint16_t size) {
 
 
 
-struct TriangleDepth {
-    float depth;
-    uint8_t index;
-};
-int compare (const void* _a, const void* _b) {
-    TriangleDepth *a = (TriangleDepth*)_a;
-    TriangleDepth *b = (TriangleDepth*)_b;
-    if (a->depth == b->depth) return 0;
-    else if (a->depth < b->depth) return -1;
-    else return 1;
-}
 
 
-// https://forums.raspberrypi.com/viewtopic.php?t=302960
-void seed_random_from_rosc()
-{
-  uint32_t random = 0x811c9dc5;
-  uint8_t next_byte = 0;
-  volatile uint32_t *rnd_reg = (uint32_t *)(ROSC_BASE + ROSC_RANDOMBIT_OFFSET);
-
-  for (int i = 0; i < 16; i++) {
-    for (int k = 0; k < 8; k++) {
-      next_byte = (next_byte << 1) | (*rnd_reg & 1);
-    }
-
-    random ^= next_byte;
-    random *= 0x01000193;
-  }
-
-  srand(random);
-} 
 
 
 
@@ -101,6 +72,7 @@ void seed_random_from_rosc()
 int8_t buf0[BUF_SIZE];
 int8_t buf1[BUF_SIZE];
 
+#include "random.h"
 //#include "testcardf.h"
 //#include "raspberrypi.h"
 #include "renderlbm.h"
@@ -166,6 +138,8 @@ int main() {
     int8_t *tbuf;
 
 
+seed_random_from_rosc();
+
 bool led = true;
 uint8_t at = 0;
 
@@ -218,7 +192,6 @@ uint8_t color[NUM_TRIS*3] = {100,   0,   0,
 TriangleDepth tridepths[NUM_TRIS];
 
 Vector3 centres[NUM_CUBES];
-seed_random_from_rosc();
 //for (uint8_t i = 0; i < NUM_CUBES; i++ ) {
 //    centres[i] = { float(rand() % 20) - 10.0f,
 //                   float(rand() % 40) - 20.0f,
