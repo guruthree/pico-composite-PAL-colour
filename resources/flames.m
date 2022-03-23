afb5f27901ed4b0a1eb8b47f823f5f8e3b40f94e
Fred <guru.three@gmail.com>
2022-03-23 23:42:20 +0000

    algorithmic generation of flame sprite

---
+++ resources/flames.m
@@ -0,0 +1,135 @@
+/*
+ *
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2022 guruthree
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ */
+
+% algorithmic generation of a flame sprite
+
+clear
+im = nan(32,32);
+
+
+y = 1;
+
+oldx = 1;
+x = randi([5 7]);
+im(y, oldx:(oldx+x)) = nan;
+
+oldx = oldx + x + 1;
+x = randi([2 3]);
+im(y, oldx:(oldx+x)) = 1;
+
+oldx = oldx + x + 1;
+x = randi([1 2]);
+im(y, oldx:(oldx+x)) = 2;
+
+oldx = oldx + x + 1;
+x = randi([2 4]);
+im(y, oldx:(oldx+x)) = 3;
+
+oldx = oldx + x + 1;
+x = randi([1 2]);
+im(y, oldx:(oldx+x)) = 2;
+
+oldx = oldx + x + 1;
+x = randi([2 3]);
+im(y, oldx:(oldx+x)) = 1;
+
+%pcolor(im)
+%return
+
+% flame outlines
+for zz=1:3
+    for y=2:32
+        if zz == 1
+            if y <= 5
+                probs = [0.6 0.7 1];
+            elseif y < 10
+                probs = [0.1 0.3 1];
+            else
+                probs = [0 0.02 1];
+            end
+        elseif zz == 2
+            if y <= 5
+                probs = [0.5 0.6 1];
+            elseif y < 10
+                probs = [0 0.3 1];
+            else
+                probs = [0 0.02 1];
+            end
+        elseif zz == 3
+            if y <= 5
+                probs = [0.3 0.4 1];
+            elseif y < 10
+                probs = [0 0.2 1];
+            else
+                probs = [0 0.02 1];
+            end
+        end
+
+        x = find(im(y-1,:) == zz, 1);
+
+        r = rand();
+        if r < probs(1) && isnan(im(y,x-1)) && isnan(im(y,x)) % expand
+            im(y,x-1) = zz;
+        elseif r < probs(2) && isnan(im(y,x)) % unchange
+            im(y,x) = zz;
+        else%if r < probs(3) % contract
+            if isnan(im(y,x+1))
+                im(y,x+1) = zz;
+            else
+                break
+            end
+        end
+
+        x = find(im(y-1,:) == zz, 1, 'last');
+        if im(y,x) == zz || im(y,x-1) == zz % shape is closed
+            break
+        end
+        r = rand();
+        if r < probs(1) && isnan(im(y,x+1)) && isnan(im(y,x)) % expand
+            im(y,x+1) = zz;
+        elseif r < probs(2) && isnan(im(y,x)) % unchange
+            im(y,x) = zz;
+        else%if r < probs(3) % contract
+            if isnan(im(y,x-1))
+                im(y,x-1) = zz;
+            else
+                break
+            end
+        end
+    end
+end
+
+% fill in flame insides
+for y=2:32
+    for x=1:find(im(y,:)==1,1,'last')
+        if ~isnan(im(y,x)) && isnan(im(y,x+1))
+            im(y,x+1) = im(y,x);
+        end
+    end
+end
+
+pcolor(im)
+colormap autumn
